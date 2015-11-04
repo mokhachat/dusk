da1172bb85e3cd0c7100570812e9dd06dec7a494
mokhachat <xreinf.fnk+github@gmail.com>
2015-11-05 00:47:20 +0900

    first

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "dusk", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
