a874071080d06458c605b6dc6a68838a963ae223
mokhachat <xreinf.fnk+github@gmail.com>
2015-11-09 03:19:57 +0900

    text texture

---
+++ shader/shader.go
@@ -0,0 +1,92 @@
+package shader
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+// Program po
+func Program(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compile(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compile(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
+
+func compile(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csource := gl.Str(source)
+	gl.ShaderSource(shader, 1, &csource, nil)
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+// VertexShader po
+const VertexShader = `
+#version 330
+uniform mat4 projection;
+uniform mat4 camera;
+uniform mat4 model;
+in vec3 vert;
+in vec2 vertTexCoord;
+out vec2 fragTexCoord;
+void main() {
+    fragTexCoord = vertTexCoord;
+    gl_Position = projection * camera * model * vec4(vert, 1);
+}
+` + "\x00"
+
+// FragmentShader po
+const FragmentShader = `
+#version 330
+uniform sampler2D tex;
+in vec2 fragTexCoord;
+out vec4 outputColor;
+void main() {
+    outputColor = texture(tex, fragTexCoord);
+}
+` + "\x00"
---
+++ texture/font.go
@@ -0,0 +1,51 @@
+package texture
+
+import (
+	"image"
+	"image/draw"
+	"io/ioutil"
+	"math"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+func Text(fontfile string, size int, text string) (uint32, error) {
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(fontfile)
+	if err != nil {
+		return 0, err
+	}
+	f, err := truetype.Parse(fontBytes)
+	if err != nil {
+		return 0, err
+	}
+
+	// Draw the background and the guidelines.
+	fg, bg := image.White, image.Transparent
+	imgW, imgH := size, size
+
+	rgba := image.NewRGBA(image.Rect(0, 0, imgW, imgH))
+	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+
+	// Draw the text.
+	dpi := 72
+	d := &font.Drawer{
+		Dst: rgba,
+		Src: fg,
+		Face: truetype.NewFace(f, &truetype.Options{
+			Size:    float64(size),
+			DPI:     float64(dpi),
+			Hinting: font.HintingNone,
+		}),
+	}
+	y := int(math.Ceil(float64(size*dpi) / 72))
+	d.Dot = fixed.Point26_6{
+		X: (fixed.I(imgW) - d.MeasureString(text)) / 2,
+		Y: fixed.I(y),
+	}
+	d.DrawString(text)
+
+	return Create(*rgba)
+}
